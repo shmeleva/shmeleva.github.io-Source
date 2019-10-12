7f00a2ef040089d0681e57e4b6cc4cc35896e692
Ekaterina Shmeleva <ekaterina.shmeleva@aalto.fi>
2019-10-13 00:03:48 +0300

    Update content

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,97 +1,125 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
+    <h1>https://rina.sh/meleva</h1>
+    <div>
+      <p>
+        Hey-hey 👋 I’m Rina Shmeleva — 👷‍♀️ full-stack web &#38; mobile app
+        developer and 👩‍🎓 Aalto University MSc student in Security and Cloud
+        Computing and Human-Computer Interaction
+      </p>
+      <p>
+        Feel like talking over a bowl of noodles? Drop me a line!
+        <br />
+        [etunimi].[sukunimi][at]ya.ru
+        <br />
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          href="https://github.com/shmeleva"
           target="_blank"
           rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          >GitHub</a
+        > * <a
+          href="https://www.linkedin.com/in/eshmeleva/"
           target="_blank"
           rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
+          >LinkedIn</a
+        > * <a
+          href="https://www.instagram.com/rina.shmeleva/"
           target="_blank"
           rel="noopener"
-          >vue-devtools</a
+          >Instagram</a
         >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+      </p>
+    </div>
+    <div>
+      <h3>Education</h3>
+      <p>
+        09/2012 – 07/2016
+        <br />
+        BSc in Mathematics and Computer Science
+        <br />
+        MESI → PRUE, Moscow, Russia
+        <br />
+        5.0 / 5.0
+      </p>
+      <p>
+        09/2018 – present
+        <br />
+        MSc in Computer, Communication and Information Sciences
+        <br />
+        Aalto, Espoo, Finland
+        <br />
+        4.9 / 5.0
+      </p>
+    </div>
+    <div>
+      <h3>Work-work-work!</h3>
+      <p>
+        02/2016 – 08/2018
+        <br />
+        Software Developer
+        <br />
+        Vestbery, Moscow, Russia
+      </p>
+      <p>
+        06/2019 – 08/2019 ☀️
+        <br />
+        Software Developer
+        <br />
+        Reaktor, Helsinki, Finland
+      </p>
+    </div>
+    <div>
+      <h3>Skills</h3>
+      <p>
+        frontend rinashmeleva$ ls
+        <br />
+        Vue.js, ReactJS
+        <br />
+        mobile rinashmeleva$ ls
+        <br />
+        React Native, Xamarin, Android
+        <br />
+        backend rinashmeleva$ ls
+        <br />
+        ASP.NET, Node.js
+        <br />
+        languages rinashmeleva$ ls
+        <br />
+        C#, JavaScript, TypeScript, Kotlin
+        <br />
+        skills rinashmeleva$ _
+      <p/>
+    </div>
+    <div>
+      <h3>Values</h3>
+      <p>
+        * Building products beautiful inside &#38; out
+        <br />
+        * Being part of a team &#38; taking personal responsibility
+        <br />
+        * Welcoming new &#38; unknown challenges
+      <p/>
+    </div>
+    <div>
+      <h3>Faves</h3>
+      <p>
+        * Taking tons of photographs &#38; never sharing
+        <br />
+        * Reading &#38; complaining about urban planning
+        <br />
+        * Exploring cities &#38; spending time in nature
+        <br />
+        * Making awkward attempts at snowboarding
+        <br />
+        * Noodles 🍜
+      <p/>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
-  }
+  name: "HelloWorld"
 };
 </script>
 
